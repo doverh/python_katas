9b101368350db58320b8029229555d61a8779f30
doverh <doverheld@gmail.com>
2019-08-01 23:59:56 -0400

    Exercise 1 to be completed

--- python_katas/exerc_1.py
+++ python_katas/exerc_1.py
@@ -3,4 +3,4 @@ name  = input("Give me your name: ")
 age = int(input("Give me your age: "))
 toHundred = str(100 - age)
 #System should inform how long to user become 100 years old
-print("You are "+toHundred+" years to be 100 years old")
+print(name+", you are "+toHundred+" years to be 100 years old")
