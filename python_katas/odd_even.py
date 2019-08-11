929cb214d7e553778b50720489cbefd7c22387b9
doverh <doverheld@gmail.com>
2019-08-11 15:23:20 -0400

    Adding python and machine learning exercises

---
+++ python_katas/odd_even.py
@@ -0,0 +1,12 @@
+# Exercise from https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+# Requirements
+# Receive an user input as integer
+# 1. Is it a number?
+# 2. Is it even? Divide by 2 with no remaider from division
+# Extra 1. Is it also divided by 4
+# Extra 2. Receive 2 numbers, first input is num, second input is the divisor, call it "check". 
+# Inform the user the appropriate result - divisible or not 
+
+#solution
+#Convert string input a
+num = input()
